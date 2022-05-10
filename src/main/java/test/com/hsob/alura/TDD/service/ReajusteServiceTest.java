c68487a226534a9bac18eb64bdbacf1bd173ae07
laranjazda <carloseng.jr@gmail.com>
2022-05-10 00:10:05 -0300

    outros recursos do junit

--- src/main/java/test/com/hsob/alura/TDD/service/ReajusteServiceTest.java
+++ src/main/java/test/com/hsob/alura/TDD/service/ReajusteServiceTest.java
@@ -2,39 +2,53 @@ package test.com.hsob.alura.TDD.service;
 
 import com.hsob.alura.TDD.model.Funcionario;
 import com.hsob.alura.TDD.service.ReadjustService;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import com.hsob.alura.TDD.model.Desempenho;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReajusteServiceTest {
+    private ReadjustService readjustService;
+    private Funcionario funcionario;
+
+    @BeforeEach /*INICIALIZA O MÉTODO ANTES DE CADA TESTE*/
+    public void iniciaVariaveis(){
+        System.out.println("Antes de cada teste");
+        this.readjustService = new ReadjustService();
+        this.funcionario = new Funcionario("Carlos", LocalDate.now(), 1000.0);
+    }
+    @AfterEach
+    public void depoisDeCadaTeste(){
+        System.out.println("Depois de cada teste");
+    }
+
+    @BeforeAll
+    public static void antesdeTodosTeste(){
+        System.out.println("ANTES DE TODOS OS TESTE");
+    }
+
+    @AfterAll
+    public static void depoisDeTodosTeste(){
+        System.out.println("DEPOIS DE TODOS OS TESTES");
+    }
 
     @Test
     public void reajusteTresPorCentoDesempenhoADesejar(){
-        ReadjustService readjustService = new ReadjustService();
-        Funcionario funcionario = new Funcionario("Carlos", LocalDate.now(), 1000.0);
-//      3% de reajuste com desempenho a desejar = 1030.00
         readjustService.concedeReadjust(funcionario, Desempenho.A_DESEJAR);
-
         assertEquals(1030.0, funcionario.getSalario());
     }
 
     @Test
     public void reajusteQuinzePorCentoDesempenhoBom(){
-        ReadjustService readjustService = new ReadjustService();
-        Funcionario funcionario = new Funcionario("Carlos", LocalDate.now(), 1000.0);
-//      15% de reajuste com desempenho bom = 1150.00
         readjustService.concedeReadjust(funcionario, Desempenho.BOM);
         assertEquals(1150.0, funcionario.getSalario());
     }
 
     @Test
     public void reajusteVintePorCentoDesempenhoOtimo(){
-        ReadjustService readjustService = new ReadjustService();
-        Funcionario funcionario = new Funcionario("Carlos", LocalDate.now(), 1000.0);
-//      15% de reajuste com desempenho otimo = 1200.00
         readjustService.concedeReadjust(funcionario, Desempenho.OTIMO);
         assertEquals(1200.0, funcionario.getSalario());
     }
