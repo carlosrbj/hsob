3136d9bea86b87d75ab61a738ef7b23d859ba4dc
laranjazda <carloseng.jr@gmail.com>
2022-06-25 00:21:43 -0300

    endpoint test

--- src/main/java/com/hsob/controller/CategoriesController.java
+++ src/main/java/com/hsob/controller/CategoriesController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -27,13 +28,16 @@ public class CategoriesController {
     }
 
     @GetMapping("get-list")
-    public ResponseEntity getCategoriesList(@RequestHeader String password,
-                                            @RequestHeader String username){
+//    public ResponseEntity<List<Category>> getCategoriesList(@RequestHeader String password,
+//                                            @RequestHeader String username){
+    public ResponseEntity<List<Category>> getCategoriesList(){
+        String username = "01551974010";
+        String password = "12345";
         try {
             List<Category> categoriesList = categoryService.getCategoriesList(password, username);
             return ResponseEntity.ok(categoriesList);
         } catch (Exception exception){
-            return ResponseEntity.internalServerError().body(exception.getMessage());
+            return ResponseEntity.internalServerError().body(new ArrayList<>());
         }
     }
 }
