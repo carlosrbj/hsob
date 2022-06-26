7c39410556a10064557e528d8dc88c1e1d9b5a19
laranjazda <carloseng.jr@gmail.com>
2022-06-26 17:57:02 -0300

    set hsob_db variable

--- src/main/java/com/hsob/repository/Anon.java
+++ src/main/java/com/hsob/repository/Anon.java
@@ -4,6 +4,6 @@ package com.hsob.repository;
  * @author carlos
  */
 public abstract class Anon{
-    public static final String BASE_MONGO_CONECTION = "mongodb+srv://laranjazda:bros2011@h-s-o-b.5b97q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
+    public static final String BASE_MONGO_CONECTION = System.getenv("hsob_db");
+//    hsob_db = "mongodb+srv://laranjazda:bros2011@h-s-o-b.5b97q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
