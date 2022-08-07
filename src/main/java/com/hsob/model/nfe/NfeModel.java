e646b3a37edff7d36f4db4027f39a0cdea2f26ff
laranjazda <carloseng.jr@gmail.com>
2022-08-07 17:58:54 -0300

    Nfe changes

--- src/main/java/com/hsob/model/nfe/NfeModel.java
+++ src/main/java/com/hsob/model/nfe/NfeModel.java
@@ -1,2 +1,12 @@
-package com.hsob.model.nfe;public class NfeModel {
+package com.hsob.model.nfe;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Setter
+public class NfeModel {
+    private String code;
+    private String name;
 }
--- src/main/java/com/hsob/service/NfeService.java
+++ src/main/java/com/hsob/service/NfeService.java
@@ -1,2 +1,32 @@
-package com.hsob.service;public class NfeService {
+package com.hsob.service;
+
+import com.hsob.model.nfe.NfeModel;
+import com.hsob.model.nfe.QrcodeModel;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.HTMLDocument;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+@Service
+public class NfeService {
+    public NfeModel readNfe(QrcodeModel qrcode) throws URISyntaxException, IOException {
+        String link = "http://www.fazenda.pr.gov.br/nfce/qrcode?p=" + qrcode.getCode();
+
+        URL url = new URL(link);
+
+        HTMLDocument htmlDocument = new HTMLDocument();
+        htmlDocument.setBase(url);
+
+        File file = new File(url.getFile());
+
+        Desktop.getDesktop().open(file);
+
+
+        return new NfeModel();
+    }
 }
